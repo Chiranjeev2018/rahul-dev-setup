e83125b77685c9ba564ed4a93102b380bb63d29e
Rahul Nanda <rahulnanda@users.noreply.github.com>
2017-11-03 13:03:37 -0700

    Update cscope_build.sh

--- cscope_build.sh
+++ cscope_build.sh
@@ -8,4 +8,5 @@ find /home/user/dev/ -name '*.c' > /home/user/dev/cscope.files
 find /home/user/dev/ -name '*.h' >> /home/user/dev/cscope.files
 find /home/user/dev/ -name '*.cc' >> /home/user/dev/cscope.files
 find /home/user/dev/ -name '*.proto' >> /home/user/dev/cscope.files
+find /home/user/dev/ -name '*.yang' >> /home/user/dev/cscope.files
 cscope -b
